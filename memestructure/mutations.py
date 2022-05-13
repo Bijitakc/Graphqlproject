3cfe78389991bed0c6580556bd9cfd4b67fcb5f0
Bijitakc <30403841+Bijitakc@users.noreply.github.com>
2022-05-13 14:58:13 +0545

    Reply schemas added

--- memestructure/mutations.py
+++ memestructure/mutations.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 from user.models import ExtendUser
 from .models import Meme, Comment, Reply
-from .types import MemeType, CommentType
+from .types import MemeType, CommentType, ReplyType
 
 
 class CreateMemeMutation(graphene.Mutation):
@@ -39,6 +39,22 @@ class CreateCommentMutation(graphene.Mutation):
         return CreateCommentMutation(comment=comment_ins)
 
 
+class CreateReplyMutation(graphene.Mutation):
+    reply = graphene.Field(ReplyType)
+
+    class Arguments:
+        authorID = graphene.ID()
+        content = graphene.String()
+        commentID = graphene.ID()
+
+    @classmethod
+    def mutate(cls, root, info, authorID, content, commentID):
+        author = ExtendUser.objects.get(id=authorID)
+        comment = Comment.objects.get(id=commentID)
+        reply_ins = Reply.objects.create(author=author, content=content, parent_comment=comment)
+        return CreateReplyMutation(reply=reply_ins)
+
+
 class UpdateMemeMutation(graphene.Mutation):
     meme = graphene.Field(MemeType)
 
@@ -67,6 +83,21 @@ class UpdateCommentMutation(graphene.Mutation):
         comment.content = content
         comment.save()
         return UpdateCommentMutation(comment=comment)
+
+
+class UpdateReplyMutation(graphene.Mutation):
+    reply = graphene.Field(ReplyType)
+
+    class Arguments:
+        id = graphene.ID()
+        content = graphene.String(required=True)
+
+    @classmethod
+    def mutate(cls, root, info, id, content):
+        reply = Reply.objects.get(id=id)
+        reply.content = content
+        reply.save()
+        return UpdateReplyMutation(reply=reply)
 
 
 class DeleteMemeMutation(graphene.Mutation):
@@ -98,7 +129,9 @@ class DeleteCommentMutation(graphene.Mutation):
 class Mutation(graphene.ObjectType):
     create_meme = CreateMemeMutation.Field()
     create_comment = CreateCommentMutation.Field()
+    create_reply = CreateReplyMutation.Field()
     update_meme_title = UpdateMemeMutation.Field()
     update_comment = UpdateCommentMutation.Field()
+    update_reply = UpdateReplyMutation.Field()
     delete_meme = DeleteMemeMutation.Field()
     delete_comment = DeleteCommentMutation.Field()
